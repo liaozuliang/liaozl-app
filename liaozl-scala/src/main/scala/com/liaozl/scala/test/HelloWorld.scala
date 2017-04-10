3eb569203c693d284c25a0b35efd444774325ea7
liaozuliang <lzlhen1988@126.com>
2017-04-10 11:50:03 +0800

    新增scala示例

---
+++ liaozl-scala/src/main/scala/com/liaozl/scala/test/HelloWorld.scala
@@ -0,0 +1,13 @@
+package com.liaozl.scala.test
+
+/**
+  *
+  * @author liaozuliang
+  * @date 2017-04-10
+  */
+object HelloWorld {
+
+    def main(args: Array[String]): Unit = {
+        println("hello, welcome to scala world!")
+    }
+}
